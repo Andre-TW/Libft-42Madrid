a6e2003b800b6d7a7421134c9c0f22797eb5f269
Andre-TW <andregabriel020@gmail.com>
2025-01-18 22:12:02 +0100

    Add files via upload
    strchr echo

---
+++ libft/ft_strchr.c
@@ -0,0 +1,29 @@
+//#include "libft.h"
+#include <stddef.h>
+char	*ft_strchr(const char *s, int c)
+{
+	char	cb;
+	
+	cb = (char)c;
+	while (*s)
+	{
+		if (*s == cb)
+		{
+			return (char *)s;
+		}
+	s++;
+	}
+	if (cb == '\0')
+	{
+		return ((char *)s);
+	}
+	return (NULL);
+}
+/*#include <stdio.h>
+int main()
+{
+	const char *tst = "hello world";
+	char ch = 'l';
+	printf("sei la '%c' %s\n", ch, ft_strchr(tst,ch));
+	return (0);
+}*/
