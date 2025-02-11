05b888c8d7730204259bdc2fe954167d70bd2f76
Andre-TW <andregabriel020@gmail.com>
2025-02-11 17:16:38 +0100

    100% working libft

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: andsoare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:54:56 by andsoare          #+#    #+#             */
-/*   Updated: 2025/02/02 14:21:38 by andsoare         ###   ########.fr       */
+/*   Updated: 2025/02/05 11:34:51 by andsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,24 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_bzero(ptr, total);
 	return (ptr);
 }
+
+/*int main() {
+    // Test with an array of 10 integers
+    int *arr = ft_calloc(10, sizeof(int));
+
+    // Check if malloc was successful
+    if (arr == NULL) {
+        printf("Memory allocation failed\n");
+        return 1;  // Return with error code
+    }
+
+    // Print values of the array to confirm they are initialized to zero
+    for (int i = 0; i < 10; i++) {
+        printf("arr[%d] = %d\n", i, arr[i]);
+    }
+
+    // Don't forget to free the memory after testing
+    free(arr);
+
+    return 0;
+}*/
--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,29 +5,109 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: andsoare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/02 17:32:37 by andsoare          #+#    #+#             */
-/*   Updated: 2025/02/02 17:38:20 by andsoare         ###   ########.fr       */
+/*   Created: 2025/02/03 15:31:56 by andsoare          #+#    #+#             */
+/*   Updated: 2025/02/05 11:39:20 by andsoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	count_str()
+static	size_t	count_str(char *str, char c)
 {
+	size_t	i;
 
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] && str[i] != c)
+	{
+		i++;
+	}
+	return (i);
 }
 
-void	ft_free()
+static size_t	ft_count_words(char *s, char c)
 {
+	size_t	i;
+	size_t	wcount;
 
+	i = 0;
+	wcount = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			wcount++;
+		i++;
+	}
+	return (wcount);
 }
 
-char	*fill_str()
+static void	ft_deallocate(char **matrix)
 {
+	size_t	str;
 
+	str = 0;
+	while (matrix[str])
+	{
+		free(matrix[str]);
+		str++;
+	}
+	free (matrix);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**matrix;
+	size_t	j;
+	size_t	num_words;
+
+	if (!s)
+		return (NULL);
+	num_words = ft_count_words((char *)s, c);
+	matrix = (char **) malloc(sizeof(char *) * (num_words + 1));
+	if (!matrix)
+		return (NULL);
+	j = 0;
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			matrix[j] = ft_substr(s, 0, count_str((char *)s, c));
+			if (!matrix[j++])
+				return (ft_deallocate(matrix), NULL);
+			s += count_str((char *)s, c);
+		}
+		else
+			s++;
+	}
+	matrix[j] = NULL;
+	return (matrix);
 }
+// for easy compile cc -w -w -w ft_split.c ft_substr.c 
+//ft_strdup.c ft_strlen.c ft_calloc.c ft_bzero.c
+/*int main() {
+    char *str = "Hello world, this is a test string";
+    char delimiter = ' ';
+
+    // Call the ft_split function
+    char **split_str = ft_split(str, delimiter);
+
+    // Check if ft_split returned NULL
+    if (split_str == NULL) {
+        printf("Memory allocation failed or invalid input.\n");
+        return 1;
+    }
+
+    // Print the words in the split string
+    printf("The split string is:\n");
+    size_t i = 0;
+    while (split_str[i]) {
+        printf("split_str[%zu] = %s\n", i, split_str[i]);
+        i++;
+    }
+
+    // Free the allocated memory
+    ft_deallocate(split_str);
+
+    return 0;
+}*/
